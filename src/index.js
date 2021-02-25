372cf3023d2e163a56830fa17e55489f03503b95
jamesinaxx <grassojames5@gmail.com>
2021-02-25 19:59:22 +1100

    Added a clock to the website cuz why not

---
+++ src/index.js
@@ -0,0 +1,33 @@
+async function setTime() {
+  const date = new Date();
+
+  // #region Hour
+  let hour = date.getHours().toString();
+
+  if (hour.length == 1) {
+    hour = "0" + hour;
+  }
+  // #endregion Hour
+
+  // #region Minute
+  let minute = date.getMinutes().toString();
+
+  if (minute.length == 1) {
+    minute = "0" + minute;
+  }
+  // #endregion Minute
+
+  // #region second
+  let second = date.getSeconds().toString();
+
+  if (second.length == 1) {
+    second = "0" + second;
+  }
+  // #endregion second
+
+  const timeVar = `${hour}:${minute}:${second}`;
+  console.log(`Set the time to ${timeVar}`);
+  $(".time").text(timeVar);
+}
+setTime();
+window.setInterval(setTime, 1000);
