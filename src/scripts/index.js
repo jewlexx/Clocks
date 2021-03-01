713fd49632d7806ed496c7ef425fa8d705a2721e
jamesinaxx <grassojames5@gmail.com>
2021-03-01 14:09:03 +1100

    Added and fixed the 24/12 hour thingo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,23 @@
-getTime()
-  .then((time) => {
-    console.log(`Set the time to ${time}`);
-    $("#time").text(time);
-  })
-  .catch((error) => {
-    console.error("Failed to set time \n" + error);
-  });
-window.setInterval(() => {
-  getTime()
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+// TODO Add 12/24 hour time toggle
+// TODO Fix the clocks
+//  Cuz like the clocks aren't actually scaling into the frame right now soooo gotta fix that
+if (urlParams.has("clock")) {
+  document
+    .getElementById("clock-style")
+    .setAttribute(
+      "href",
+      document
+        .getElementById("clock-style")
+        .getAttribute("href")
+        .replace("basic", urlParams.get("clock"))
+    );
+  console.log("Set the clock to " + urlParams.get("clock"));
+}
+
+if (urlParams.has("24hour") && urlParams.get("24hour") == "true") {
+  getTime24()
     .then((time) => {
       console.log(`Set the time to ${time}`);
       $("#time").text(time);
@@ -15,4 +25,33 @@ window.setInterval(() => {
     .catch((error) => {
       console.error("Failed to set time \n" + error);
     });
-}, 16.66666666);
+  window.setInterval(() => {
+    getTime24()
+      .then((time) => {
+        console.log(`Set the time to ${time}`);
+        $("#time").text(time);
+      })
+      .catch((error) => {
+        console.error("Failed to set time \n" + error);
+      });
+  }, 1000);
+} else {
+  getTime12()
+    .then((time) => {
+      console.log(`Set the time to ${time}`);
+      $("#time").text(time);
+    })
+    .catch((error) => {
+      console.error("Failed to set time \n" + error);
+    });
+  window.setInterval(() => {
+    getTime12()
+      .then((time) => {
+        console.log(`Set the time to ${time}`);
+        $("#time").text(time);
+      })
+      .catch((error) => {
+        console.error("Failed to set time \n" + error);
+      });
+  }, 1000);
+}
---
+++ src/scripts/time.js
@@ -0,0 +1,61 @@
+async function getTime12() {
+  let am = "AM";
+  const date = new Date();
+
+  // #region Hour
+  let hour = date.getHours();
+
+  if (hour > 12) {
+    hour -= 12;
+    am = "PM";
+  }
+  // #endregion Hour
+
+  // #region Minute
+  let minute = date.getMinutes().toString();
+
+  if (minute.length == 1) {
+    minute = "0" + minute;
+  }
+  // #endregion Minute
+
+  // #region second
+  let second = date.getSeconds().toString();
+
+  if (second.length == 1) {
+    second = "0" + second;
+  }
+  // #endregion second
+
+  return `${hour}:${minute}:${second} ${am}`;
+}
+
+async function getTime24() {
+  const date = new Date();
+
+  // #region Hour
+  let hour = date.getHours().toString();
+
+  if (hour.length == 1) {
+    hour = "0" + hour;
+  }
+  // #endregion Hour
+
+  // #region Minute
+  let minute = date.getMinutes().toString();
+
+  if (minute.length == 1) {
+    minute = "0" + minute;
+  }
+  // #endregion Minute
+
+  // #region second
+  let second = date.getSeconds().toString();
+
+  if (second.length == 1) {
+    second = "0" + second;
+  }
+  // #endregion second
+
+  return `${hour}:${minute}:${second}`;
+}
