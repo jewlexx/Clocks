e091d939bd8c42d214b770c7753f1754064415d9
jamesinaxx <grassojames5@gmail.com>
2021-03-04 21:11:33 +1100

    Moved todos

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,12 @@
+// TODO Fix the clocks
+//  Cuz like the clocks aren't actually scaling into the frame right now soooo gotta fix that
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 import dayjs from "dayjs";
 import $ from "jquery";
 
-if (urlParams.has("clock")) {
-  document
-    .getElementById("clock-style")
-    .setAttribute(
-      "href",
-      document
-        .getElementById("clock-style")
-        .getAttribute("href")
-        .replace("basic", urlParams.get("clock"))
-    );
-  console.log("Set the clock to " + urlParams.get("clock"));
-}
-
-// TODO Fix the clocks
-//  Cuz like the clocks aren't actually scaling into the frame right now soooo gotta fix that
+//#region Params
 if (urlParams.has("clock")) {
   document
     .getElementById("clock-style")
@@ -33,8 +22,26 @@ if (urlParams.has("clock")) {
 
 let format = "h:mm:ss A";
 
-if (urlParams.has("24hour") && urlParams.get("24hour") == "true") {
+if (urlParams.has("24hour") && urlParams.get("24hour") === "true") {
   format = "HH:mm:ss";
+}
+
+if (urlParams.has("seconds") && urlParams.get("seconds") === "false") {
+  format = format.replace(":ss", "");
+}
+//#endregion Params
+
+if (urlParams.has("clock")) {
+  document
+    .getElementById("clock-style")
+    .setAttribute(
+      "href",
+      document
+        .getElementById("clock-style")
+        .getAttribute("href")
+        .replace("basic", urlParams.get("clock"))
+    );
+  console.log("Set the clock to " + urlParams.get("clock"));
 }
 
 $("#time").text(dayjs().format(format));
