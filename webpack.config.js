ae278a4ff3c8ad6f8c85a2eca567f1e0b18b3da2
jamesinaxx <grassojames5@gmail.com>
2021-03-04 20:53:18 +1100

    ugh

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/scripts/index.js",
-  watch: true,
+  watch: false,
   mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Clocks",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
