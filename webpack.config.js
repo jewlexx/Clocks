17ef791da49e7ef1c63ccc41d07c8592ee318d3b
jamesinaxx <grassojames5@gmail.com>
2021-03-04 20:55:53 +1100

    Updated some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/scripts/index.js",
   watch: false,
   mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Clocks",
-    }),
-  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
