41ddeab005868c0e8386e64a1fe25dc7b54e6134
jamesinaxx <grassojames5@gmail.com>
2021-03-04 20:58:53 +1100

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
+const dev = true;
+
+function devMode() {
+  if (dev) {
+    return "development";
+  } else {
+    return "production";
+  }
+}
 
 module.exports = {
   entry: "./src/scripts/index.js",
-  watch: false,
-  mode: "development",
+  watch: dev,
+  mode: devMode(),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
