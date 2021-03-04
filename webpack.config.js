062726ca2f6a5421814ca7deb8e4a64297128d51
jamesinaxx <grassojames5@gmail.com>
2021-03-04 20:33:30 +1100

    Added webpack stuff and yuh

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/scripts/index.js",
+  watch: true,
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+
+    publicPath: "/",
+    publicPath: "/Clocks/",
+  },
+};
