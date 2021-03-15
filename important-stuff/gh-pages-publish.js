b71b4c33530a60c00a66f5bbf4c877d49412d56d
jamesinaxx <grassojames5@gmail.com>
2021-03-16 09:07:14 +1100

    Removed jenkinsfile

--- important-stuff/gh-pages-publish.js
+++ important-stuff/gh-pages-publish.js
@@ -10,8 +10,6 @@ ghpages.publish(
       email: "grassojames5@gmail.com",
     },
     message: "Built and deployed github pages",
-    repo: `https://github.com/jamesinaxx/Clocks.git`,
-    remote: "origin",
   },
   function (err) {
     console.error(err);
