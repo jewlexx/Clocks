2113add71d309ed12e413aa7e1957f976229cd13
jamesinaxx <grassojames5@gmail.com>
2021-03-16 22:27:46 +1100

    Renamed clock element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Time } from './javascript/components/Clock';
+import { Clock } from './javascript/components/Clock';
 import { Generator } from './javascript/components/Generator/Generator';
 import { UrlParameters } from './javascript/imports/params';
 import './styles/index.scss';
@@ -14,7 +14,7 @@ export const clock = params.clock;
 console.debug(window.location.href);
 
 if (params.clockEnabled) {
-    ReactDOM.render(<Time />, document.getElementById('root'));
+    ReactDOM.render(<Clock />, document.getElementById('root'));
 } else {
     ReactDOM.render(<Generator />, document.getElementById('root'));
 }
--- src/javascript/components/Clock.jsx
+++ src/javascript/components/Clock.jsx
@@ -8,7 +8,7 @@ const formatTime = () => {
     }
 };
 
-class Time extends React.Component {
+class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
