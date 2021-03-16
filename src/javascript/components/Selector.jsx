28e9cafba293af5b502e0d70769e89df78830aa4
jamesinaxx <grassojames5@gmail.com>
2021-03-16 18:16:52 +1100

    Fixed readme and updated selector with a clock array

--- src/javascript/components/Selector.jsx
+++ src/javascript/components/Selector.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+const clockList = ['basic', 'pride', 'black', 'white', 'transparent'];
 
 class SelectClock extends React.Component {
     render() {
         return (
-            <button>
-                Selector coming soon... For now checkout the{' '}
-                <a href="https://github.com/jamesinaxx/Clocks/blob/public/README.md">
-                    README
-                </a>
-                for instructions
-            </button>
+            <div className="SelectClock">
+                <button>
+                    Selector coming soon... For now checkout the{' '}
+                    <a href="https://github.com/jamesinaxx/Clocks/blob/public/README.md">
+                        README
+                    </a>
+                    for instructions
+                </button>
+            </div>
         );
     }
 }
