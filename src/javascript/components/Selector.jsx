1c79198efd2c3a8f6bc65698872a9c8ad502432f
jamesinaxx <grassojames5@gmail.com>
2021-03-16 18:50:12 +1100

    Turned SelectClock into a function element

--- src/javascript/components/Selector.jsx
+++ src/javascript/components/Selector.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 const clockList = ['basic', 'pride', 'black', 'white', 'transparent'];
 
-class SelectClock extends React.Component {
-    render() {
-        document.getElementById('root').classList = 'dark-mode';
-        return (
-            <div className="SelectClock">
-                {clockList.map((item) => (
-                    <button key={clockList.indexOf(item)} className="btn-clock">
-                        {item.substring(0, 1).toUpperCase() + item.slice(1)}
-                    </button>
-                ))}
-                <button className="btn-clock" style={{ cursor: 'progress' }}>
-                    More coming soon
+function SelectClock() {
+    document.getElementById('root').classList = 'dark-mode';
+    return (
+        <div className="SelectClock">
+            {clockList.map((item) => (
+                <button key={clockList.indexOf(item)} className="btn-clock">
+                    {item.substring(0, 1).toUpperCase() + item.slice(1)}
                 </button>
-            </div>
-        );
-    }
+            ))}
+            <button className="btn-clock" style={{ cursor: 'progress' }}>
+                More coming soon
+            </button>
+        </div>
+    );
 }
 
 class Selector extends React.Component {
