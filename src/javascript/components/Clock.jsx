08c55ef94b437377cda50bf203995bb150762671
jamesinaxx <grassojames5@gmail.com>
2021-03-17 20:02:48 +1100

    Updates

--- src/javascript/components/Clock.jsx
+++ src/javascript/components/Clock.jsx
@@ -2,42 +2,16 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 import React from 'react';
 import dayjs from 'dayjs';
-import { timeFormat } from '../../index.js';
 
-class UrlParameters {
-    constructor() {
-        this.color = '#000';
-        this.bgColor = '#FFF';
-        this.clockEnabled = queryString !== '';
-        this.format = 'h:mm:ss A';
+let timeFormat = 'HH:mm:ss';
 
-        if (urlParams.has('24hour') && urlParams.get('24hour') === 'true') {
-            this.format = 'HH:mm:ss';
-        }
-
-        if (urlParams.has('seconds') && urlParams.get('seconds') === 'false') {
-            this.format = this.format.slice().replace(':ss', '');
-        }
-
-        if (urlParams.has('bgcolor')) {
-            if (urlParams.get('bgcolor') === '**pride**') {
-                this.bgColor = '#FFF';
-                document.getElementById('root').style.backgroundColor =
-                    'linear-gradient(180deg, #fe0000 16.66%, #fd8c00 16.66%, 33.32%, #ffe500 33.32%, 49.98%, #119f0b 49.98%, 66.64%, #0644b3 66.64%, 83.3%, #c22edc 83.3%);';
-            } else {
-                this.bgColor = '#' + urlParams.get('bgcolor');
-            }
-        }
-    }
+if (urlParams.has('24hour') && urlParams.get('24hour') === 'true') {
+    timeFormat = 'HH:mm:ss';
 }
 
-const params = new UrlParameters();
-
-const formatTime = () => {
-    if (timeFormat !== undefined) {
-        return dayjs().format(timeFormat);
-    }
-};
+if (urlParams.has('seconds') && urlParams.get('seconds') === 'false') {
+    timeFormat = this.format.slice().replace(':ss', '');
+}
 
 const getColor = () => {
     let colorParam = urlParams.get('clock') !== 'black' ? '#000' : '#FFF';
@@ -51,9 +25,16 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: formatTime(),
+            time: dayjs().format(timeFormat),
         };
-        setInterval(() => this.setState({ time: formatTime() }), 1000);
+        setInterval(
+            () => this.setState({ time: dayjs().format(timeFormat) }),
+            1000
+        );
+
+        if (queryString !== '') {
+            document.getElementById('root').classList = 'clock';
+        }
 
         if (urlParams.has('bgcolor')) {
             if (urlParams.get('bgcolor') === '**pride**') {
