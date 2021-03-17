27c1de5de7c86a5e080a6b93622428e28f4ea3b3
jamesinaxx <grassojames5@gmail.com>
2021-03-17 11:04:22 +1100

    Updates

--- src/javascript/components/Generator/lib/ColorInput.jsx
+++ src/javascript/components/Generator/lib/ColorInput.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
-function ColorInput(props) {
-    return (
-        <div>
-            <label className="color-input">
-                Color:{' '}
-                <input
-                    type="text"
-                    name="color-text-input"
-                    id="clock-color-input"
-                ></input>{' '}
-            </label>
-        </div>
-    );
+class ColorInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <label className="color-input">
+                    Color:{' '}
+                    <input
+                        type="text"
+                        id="clock-color-input"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    ></input>{' '}
+                </label>
+            </div>
+        );
+    }
 }
 
 export { ColorInput };
