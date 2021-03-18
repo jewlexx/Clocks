286cfaf31a55c9e0484bdc9a6f4171ffb099b5e7
jamesinaxx <grassojames5@gmail.com>
2021-03-18 13:55:33 +1100

    Fixed react

--- building/webpack.config.js
+++ building/webpack.config.js
@@ -7,7 +7,7 @@ const path = require('path');
  * @type {Configuration}
  */
 const config = {
-    entry: [path.resolve(__dirname, '../', 'src/index.js')],
+    entry: [path.resolve(__dirname, '../', 'src/index.ts')],
     stats: {
         errorDetails: true,
     },
