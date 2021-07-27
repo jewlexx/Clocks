374f93a40a5400e3a6c5cae77e43259419f224c3
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-27 21:44:34 +1000

    Fixed build script

--- publish.js
+++ publish.js
@@ -3,23 +3,23 @@ require('dotenv').config();
 
 clean();
 publish(
-    'dist',
-    {
-        user: {
-            name: 'jamesinaxx',
-            email: 'grassojames5@gmail.com',
-        },
-        message: 'Built and deployed github pages',
-        remote: 'origin',
-    },
-    function (err) {
-        if (err === undefined) {
-            console.log('Successfully published to Github pages!');
-        } else {
-            console.error(
-                "Failed to publish to Github pages... and here's why \n" + err
-            );
-            process.exit(1);
-        }
-    }
+	'dist',
+	{
+		user: {
+			name: 'jamesinaxx',
+			email: '61040004+jamesinaxx@users.noreply.github.com',
+		},
+		message: 'Built and deployed github pages',
+		remote: 'origin',
+	},
+	function (err) {
+		if (err === undefined) {
+			console.log('Successfully published to Github pages!');
+		} else {
+			console.error(
+				"Failed to publish to Github pages... and here's why \n" + err
+			);
+			process.exit(1);
+		}
+	}
 );
