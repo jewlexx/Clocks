73801ac64c2d230effeec4d473d3360006ba3644
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-30 23:00:29 +1000

    Moved to nextjs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app';
+import '@styles/global.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<div id='root'>
+			<Component {...pageProps} />
+		</div>
+	);
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Generator from '@components/Generator/Generator';
+
+export default function Home() {
+	return <Generator />;
+}
