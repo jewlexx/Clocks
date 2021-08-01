29088b00a7c7b738c1524fbf388d77cb4a5b354b
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-01 21:29:38 +1000

    Added more icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,9 @@ export default class Doc extends Document {
             name='description'
             content='A simple, highly customizable Clock, designed for performance, embedding, and ease of use in mind'
           />
+          <link rel='icon' href='/favicon.ico' sizes='any' />
+          <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         </Head>
         <body>
           <Main />
