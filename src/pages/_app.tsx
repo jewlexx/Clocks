25a270bc5ea8e4508d85c131a0d8ed8ddede703b
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-01 20:21:35 +1000

    Added meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import '@styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<div id='root'>
+			<Head>
+				<meta
+					name='description'
+					content='A simple, highly customizable Clock, designed for performance, embedding, and ease of use in mind'
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</div>
 	);
