745cf5d009c1b8c2ee9b3d9a00479d86c1bb5bd7
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-01 22:23:59 +1000

    Fixed a couple things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,10 @@
 import React from 'react';
-import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import '@styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <div id='root'>
-      <Head>
-        <meta charSet='utf-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
-        />
-        <meta
-          name='description'
-          content='A simple, highly customizable Clock, designed for performance, embedding, and ease of use in mind'
-        />
-        <meta
-          name='keywords'
-          content='clock, embed, api, easy, simple, iframe'
-        />
-        <meta name='theme-color' content='#4e54c8' />
-
-        <link rel='manifest' href='/manifest.json' />
-        <link
-          href='/icons/icon-16.png'
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-        />
-        <link
-          href='/icons/icon-32.png'
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-        />
-        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
-      </Head>
       <Component {...pageProps} />
     </div>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,32 @@ export default class Doc extends Document {
   render(): JSX.Element {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <meta
+            name='description'
+            content='A simple, highly customizable Clock, designed for performance, embedding, and ease of use in mind'
+          />
+          <meta
+            name='keywords'
+            content='clock, embed, api, easy, simple, iframe'
+          />
+          <meta name='theme-color' content='#4e54c8' />
+
+          <link rel='manifest' href='/manifest.webmanifest' />
+          <link
+            href='/icons/icon-16.png'
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+          />
+          <link
+            href='/icons/icon-32.png'
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+          />
+          <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
+        </Head>
         <body>
           <Main />
           <NextScript />
