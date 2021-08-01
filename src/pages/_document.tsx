4ef59bb625f2dbc0d5c56e4e5515a74f82a8107c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-01 22:19:03 +1000

    Added lang prop to html

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from 'next/document';
+
+export default class Doc extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
