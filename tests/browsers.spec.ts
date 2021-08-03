590ecd7d4e4b58c132d4862694735d2dbc49c01c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-03 17:01:34 +1000

    Fixed things

--- tests/browsers.spec.ts
+++ tests/browsers.spec.ts
@@ -1,24 +1,48 @@
-import { chromium, firefox, webkit } from 'playwright';
+import { chromium, firefox, webkit, devices } from 'playwright';
 import { resolve } from 'path';
+
+const iPhone = devices['iPhone 11'];
 
 const browsers = [chromium, firefox, webkit];
 
 browsers.forEach(async browser => {
   const bw = await browser.launch();
-  const ctx = await bw.newContext();
+  const deskctx = await bw.newContext();
 
-  const page = await ctx.newPage();
+  let page = await deskctx.newPage();
 
   await page.goto('https://clocks.jamesinaxx.me');
 
   await page.screenshot({
-    path: resolve(__dirname, `images/${browser.name()}/clockImage.png`),
+    path: resolve(__dirname, `images/desk${browser.name()}/clockImage.png`),
   });
 
   await page.goto(`file://${resolve(__dirname, 'transparentClock.html')}`);
 
   await page.screenshot({
-    path: resolve(__dirname, `images/${browser.name()}/transparentImage.png`),
+    path: resolve(
+      __dirname,
+      `images/desk${browser.name()}/transparentImage.png`
+    ),
+  });
+
+  const iphonectx = await bw.newContext(iPhone);
+
+  page = await iphonectx.newPage();
+
+  await page.goto('https://clocks.jamesinaxx.me');
+
+  await page.screenshot({
+    path: resolve(__dirname, `images/phone${browser.name()}/clockImage.png`),
+  });
+
+  await page.goto(`file://${resolve(__dirname, 'transparentClock.html')}`);
+
+  await page.screenshot({
+    path: resolve(
+      __dirname,
+      `images/phone${browser.name()}/transparentImage.png`
+    ),
   });
 
   bw.close();
