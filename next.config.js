5a23574e68d8b6e7de166965a237358258ff8b14
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-04 08:45:16 +1000

    Fixed the dumbest problem

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const runtimeCaching = require('next-pwa/cache');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: provess.env.NODE_ENV !== 'production',
+    disable: process.env.NODE_ENV !== 'production',
     runtimeCaching,
   },
 });
