f43e9f8396901f6ecfbe8741d5c4b1870101007c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-03 20:06:32 +1000

    Much improved the clock element

--- src/pages/clock.tsx
+++ src/pages/clock.tsx
@@ -1,69 +1,65 @@
 import React from 'react';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import dayjs from 'dayjs';
 import styles from '@styles/modules/clock.module.scss';
 import type { ClockState } from '@typings/Clock';
 
-export default class Clock extends Component<null, ClockState> {
-  constructor(props: null) {
-    super(props);
-    this.state = {
-      time: '00:00:00',
-      color: '#000',
-    };
-  }
+export default function Clock(): JSX.Element {
+  const [config, setConfig] = useState<ClockState>({
+    time: '00:00:00',
+    color: '#000',
+  });
 
-  componentDidMount(): void {
+  useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
     const timeFormat = urlParams.get('format') || 'h:mm:ss A';
 
-    this.setState({ time: dayjs().format(timeFormat) });
-
     const docRoot = document.getElementById('__next') as HTMLDivElement;
 
-    setInterval(
-      () => this.setState({ time: dayjs().format(timeFormat) }),
-      1000
-    );
-
-    if (queryString !== '') {
-      docRoot.classList.add(styles.clock);
+    if (queryString !== '' || urlParams.get('clock') === 'default') {
+      return docRoot.classList.add(styles.clock);
     }
 
-    if (urlParams.has('bgColor')) {
-      if ((urlParams.get('bgColor') as string).includes('**')) {
-        docRoot.classList.add(
-          styles[
-            (urlParams.get('bgColor') as string).replace(/\*\*/g, '') + '-clock'
-          ]
-        );
-      } else {
-        docRoot.style.backgroundColor = '#' + urlParams.get('bgColor');
-      }
+    if (urlParams.get('clock') !== 'custom') {
+      docRoot.classList.add(
+        styles[(urlParams.get('bgColor') as string) + '-clock']
+      );
+    } else {
+      docRoot.style.backgroundColor = '#' + urlParams.get('bgColor');
     }
 
     let colorParam = urlParams.get('clock') !== 'black' ? '#000' : '#FFF';
     if (urlParams.has('color')) {
       colorParam = '#' + urlParams.get('color');
     }
-    this.setState({ color: colorParam });
-
     document.body.classList.add(styles.timeDiv);
-  }
 
-  render(): JSX.Element {
-    return (
-      <main className={styles.clock}>
-        <Head>
-          <title>Clock - Embed this URL</title>
-        </Head>
-        <p className={styles.time} style={{ color: this.state.color }}>
-          {this.state.time}
-        </p>
-      </main>
-    );
-  }
+    const newConfig: ClockState = { ...config };
+
+    newConfig.color = colorParam;
+
+    newConfig.time = dayjs().format(timeFormat);
+
+    setConfig(newConfig);
+
+    setInterval(() => {
+      const newTimeConfig: ClockState = { ...config };
+      newTimeConfig.time = dayjs().format(timeFormat);
+      setConfig(newTimeConfig);
+    }, 1000);
+  });
+
+  return (
+    <main className={styles.clock}>
+      <Head>
+        <title>Clock - Embed this URL</title>
+      </Head>
+      <p className={styles.time} style={{ color: config.color }}>
+        {config.time}
+      </p>
+    </main>
+  );
 }
