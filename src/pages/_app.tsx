17d1d9ab81e370d5796e6ebce0830d65b431226c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-12 19:14:24 +1000

    Fixed pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import '@styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+      });
+    }
+  });
+
   return <Component {...pageProps} />;
 }
