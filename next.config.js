f4670582e1036211986e2e47ff782c1e13554900
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-08-13 08:27:09 +1000

    Fixed deps

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 // TODO Add service worker
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
 // Idk if this should work or if there is a better way but it does work so yea
 function customBuildSteps() {
   require('./scripts/icons.js');
 }
-
-const pwaconfig = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV !== 'production',
-    runtimeCaching,
-  },
-});
 
 const config = {};
 
