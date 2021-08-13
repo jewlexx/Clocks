19cb71527e865212505250844dacb209e619dded
James Cordor <61040004+jamesinaxx@users.noreply.github.com>
2021-08-13 12:34:15 +1000

    Added gtag script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
@@ -11,5 +12,13 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     }
   });
 
-  return <Component {...pageProps} />;
+  return <div><Head><!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-79T6JHXJ9E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-79T6JHXJ9E');
+</script></Head><Component {...pageProps} /></div>;
 }
