aefca60d35b763dab9ae8503323ca21ad59256dd
Juliette Cordor <me@cordor.dev>
2025-02-23 20:18:32 +1100

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+/** @type {import('prettier').Options} */
+const config = {
+  printWidth: 80,
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  requirePragma: false,
+  rangeEnd: Infinity,
+  rangeStart: 0,
+};
+
+module.exports = config;
